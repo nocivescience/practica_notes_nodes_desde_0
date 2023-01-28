62bb10e72c341d6be975a4044e6b4910d30f4fa3
noceive <nocivescience@gmail.com>
2023-01-28 18:43:30 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express=require('express')
+const path=require('path')
+const morgan= require('morgan')
+const {engine}= require('express-handlebars')
+const app=express()
+//settings
+app.set('port', process.env.PORT||3000)
+app.set('views', path.join(__dirname,'views')) 
+app.engine(
+    '.hbs',
+    engine({
+        defaultLayout:'main',
+        layoutsDir: path.join(app.get('views'), 'layouts'),
+        partialsDir: path.join(app.get('views'), 'partials'),
+        extname: '.hbs',
+    })
+)
+app.set('view engine', '.hbs')
+//middlewares
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended:true}))
+app.use(require('./routes/notes.routes'))
+app.use(express.static(path.join(app.get('views'),'static')))
+//listeners
+app.listen(app.get('port'),()=>{
+    console.log(`la conexion esta en el puerto ${app.get('port')}`)
+})
